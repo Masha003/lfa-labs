e2d2e6592d9ed6aa6ce03f068bacc4c1b817067e
Masha003 <colta.maha@gmail.com>
2024-03-01 19:53:22 +0200

    Added the project in js

---
+++ RegularGrammar/main.js
@@ -0,0 +1,16 @@
+import { Grammar } from "./Grammar.js";
+
+const grammar = new Grammar();
+const fa = grammar.to_finite_automaton();
+
+let testStrings = ["jaeed", "afje"];
+
+for (let i = 0; i < 5; i++) {
+  testStrings.push(grammar.generate_string());
+}
+
+console.log(`Generated strings: ${testStrings}`);
+
+testStrings.forEach((s) => {
+  console.log(`String '${s}' is accepted: ${fa.accept(s)}`);
+});
