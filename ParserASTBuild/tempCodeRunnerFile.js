1f9243119b27b9bfce86fb7d202fa759e0ef988e
Masha003 <colta.maha@gmail.com>
2024-04-28 16:21:16 +0300

    Updated readme file

--- ParserASTBuild/tempCodeRunnerFile.js
+++ ParserASTBuild/tempCodeRunnerFile.js
@@ -1,6 +1,11 @@
+const input = "12 + (variableName - 34) * 56 / variable_2";
+
+const lexer = new Lexer(input);
+const tokens = lexer.tokenize();
+
+console.log(tokens);
 
 const parser = new Parser(tokens);
 const ast = parser.parseExpression();
 
-// console.log(JSON.stringify(ast, null, 2));
-console.log(ast);
+console.log(JSON.stringify(ast, null, 2));
